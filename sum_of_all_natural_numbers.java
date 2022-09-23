ddb9cc34df982b49131c39c33a0353d67803bc79
Yashwant361 <93814966+Yashwant361@users.noreply.github.com>
2022-09-23 21:50:52 +0530

    Create sum_of_all_natural_numbers.java
    The while loop executes until the condition i <= num do not become false. It calculates the sum of natural numbers up to a specified limit.

---
+++ sum_of_all_natural_numbers.java
@@ -0,0 +1,15 @@
+public class Sumofnaturalnumbers {
+
+	public static void main(String[] args) {
+	
+		        int num = 50, i = 1, sum = 0;
+
+		        while(i <= num)
+		        {
+		            sum += i;
+		            i++;
+		        }
+
+		        System.out.println("Sum = " + sum);
+		    }
+		}
