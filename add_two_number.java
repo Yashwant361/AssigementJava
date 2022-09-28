317a223b90b8959ac5f2a4dfca9aac1cc7de8058
Yashwant361 <93814966+Yashwant361@users.noreply.github.com>
2022-09-28 23:43:26 +0530

    Create add_two_number.java

---
+++ add_two_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Add{
+  public static void main(String[] args){
+  //  int sum;
+Scanner in= new Scanner(System.in);
+    System.out.print("Enter first number");
+    int x=in.nextInt();
+    System.out.print("Enter second number");
+    int y=in.nextInt();
+    
+  int sum=x+y;
+    System.out.println("Total sum is:"+sum);
+  }
+}
